530652846ada4933bf7e313c2e95884a760f694b
JairoHervert <158542532+JairoHervert@users.noreply.github.com>
2025-01-11 18:33:42 -0600

    DELETE agreagado

--- src/WebServer.java
+++ src/WebServer.java
@@ -1,3 +1,6 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -136,11 +139,12 @@ public class WebServer {
             
             int bytesRead;
             int totalBytesRead = 0;
-            while ((bytesRead = clientChannel.read(bufferByte)) > 0) {
+            while ((bytesRead = clientChannel.read(bufferByte)) > 0 || bufferByte.position() > 0) {
                bufferByte.flip();
                bufferBAOS.write(bufferByte.array(), 0, bytesRead);
                bufferByte.clear();
                totalBytesRead += bytesRead;
+               System.out.println("totalBytesRead = " + totalBytesRead);
             }
             
             if (bytesRead == -1) {
@@ -151,7 +155,7 @@ public class WebServer {
             
             // 2. Procesar la solicitud HTTP (básico)
             ByteBuffer buffer = ByteBuffer.wrap(bufferBAOS.toByteArray());
-            String request = new String(buffer.array(), 0, buffer.limit(), StandardCharsets.UTF_8);
+            String request = new String(buffer.array(), StandardCharsets.UTF_8);
             System.out.println("Solicitud recibida:\n" + request);
             
             // Separamos la solicitud en dos partes: las cabeceras y el cuerpo
@@ -160,7 +164,7 @@ public class WebServer {
             String body = requestParts.length > 1 ? requestParts[1] : "";
             
             ByteArrayOutputStream bodyBuffer = new ByteArrayOutputStream();
-            bodyBuffer.write(bufferBAOS.toByteArray(), bufferBAOS.toString().indexOf("\r\n\r\n") + 4, totalBytesRead - bufferBAOS.toString().indexOf("\r\n\r\n") - 4);
+            bodyBuffer.write(bufferBAOS.toByteArray(), bufferBAOS.toString(StandardCharsets.UTF_8).lastIndexOf("\r\n\r\n") + 4, totalBytesRead - bufferBAOS.toString().lastIndexOf("\r\n\r\n") - 4);
             
             String[] headerLines = headers.split("\r\n");   // separa todas las cabeceras en String individuales
             
@@ -188,37 +192,17 @@ public class WebServer {
                   break;
                
                case "PUT":
-                  responseBody = "Hello desde un PUT del servidor!";
-                  httpResponse = ""
-                          + "HTTP/1.1 200 OK\r\n"
-                          + "Content-Type: text/plain\r\n"
-                          + "Content-Length: " + responseBody.length() + "\r\n"
-                          + "Connection: close\r\n"
-                          + "\r\n"
-                          + responseBody;
+                  httpResponse = putHandler(request, resource, bodyBuffer);
                   
                   break;
                   
                case "DELETE":
-                  responseBody = "Hello desde un DELETE del servidor!";
-                  httpResponse = ""
-                          + "HTTP/1.1 200 OK\r\n"
-                          + "Content-Type: text/plain\r\n"
-                          + "Content-Length: " + responseBody.length() + "\r\n"
-                          + "Connection: close\r\n"
-                          + "\r\n"
-                          + responseBody;
+                  httpResponse = deleteHandler(resource);
                   break;
                   
                   
                case "HEAD":
-                  System.out.println("Si entre al head");
-                  httpResponse = ""
-                          + "HTTP/1.1 200 OK\r\n"
-                          + "Content-Type: text/plain\r\n"
-                          + "Content-Length: " + responseBody.length() + "\r\n"
-                          + "Connection: close\r\n"
-                          + "\r\n";
+                  httpResponse = headHandler(resource);
                   break;
                   
                default:
@@ -500,6 +484,302 @@ public class WebServer {
       
       return response;
    }
+   
+   // La petición HTTP PUT se utiliza para enviar datos al servidor para que procese una acción específica, es una petición idempotente.
+   // Generalmente, la URL (o URI) especificada en la petición indica la ubicación exacta del recurso que se está creando o actualizando.
+   // Los datos enviados al servidor, usualmente en el cuerpo de la solicitud, deben estar en un formato que el servidor pueda interpretar, json por ejemplo.
+   // Ej: Actualizar un recurso, reemplazar un recurso, crear un recurso si no existe, etc.
+   public static String putHandler(String request, String resource, ByteArrayOutputStream bodyBuffer) {
+      String response = "";
+      String bodyRequest = "";
+      
+      // Si la petición no tiene cuerpo se envía un mensaje de error
+      if (bodyBuffer.size() < 1) {
+         bodyRequest = "Peticion PUT sin cuerpo";
+         response = createHead(400, "text/plain", bodyRequest.length());
+         response += bodyRequest;
+         return response;
+      }
+      
+      // Extraemos el recurso solicitado
+      resource = resource.substring(1);
+      System.out.println("resource = " + resource);
+      
+      // si el recurso es un directorio se envía un mensaje de error
+      File fileResource = new File(resource);
+      if (fileResource.isDirectory()) {
+         bodyRequest = "No se puede actualizar un directorio";
+         response = createHead(400, "text/plain", bodyRequest.length());
+         response += bodyRequest;
+         return response;
+      }
+      
+      // Extraer el Content-Type de la petición
+      String contentType = "";
+      String[] requestParts = request.split("\r\n");
+      for (String part : requestParts) {
+         if (part.contains("Content-Type")) contentType = part.split(":")[1].trim();
+      }
+      
+      // verificar que el recurso y el Content-Type coincidan
+      String extension = resource.substring(resource.lastIndexOf(".") + 1);
+      if (!contentType.equals(MIME_TYPES.get(extension))) {
+         System.out.println("El recurso y el Content-Type no coinciden");
+         bodyRequest = "El recurso y el Content-Type no coinciden";
+         response = createHead(400, "text/plain", bodyRequest.length());
+         response += bodyRequest;
+         return response;
+      }
+      
+      // Si el Content-Type es Json, se verifica que el contenido sea válido
+      String json = new String(bodyBuffer.toByteArray(), StandardCharsets.UTF_8);
+      if (contentType.equals("application/json")) {
+         if (!isValidJson(json)) {
+            bodyRequest = "JSON mal formado";
+            response = createHead(400, "text/plain", bodyRequest.length());
+            response += bodyRequest;
+            return response;
+         }
+      }
+      
+      // si el archivo no existe lo creamos
+      if (!fileResource.exists()) {
+         try {
+            fileResource.createNewFile();
+            System.out.println("Archivo creado: " + fileResource.getName());
+         } catch (IOException e) {
+            e.printStackTrace();
+            bodyRequest = "Error al crear el archivo";
+            response = createHead(500, "text/plain", bodyRequest.length());
+            response += bodyRequest;
+            return response;
+         }
+      }
+      
+      // Guardar el contenido del archivo
+      try {
+         saveFile(resource, bodyBuffer.toByteArray());
+         bodyRequest = "Archivo actualizado";
+         response = createHead(200, "text/plain", bodyRequest.length());
+         response += bodyRequest;
+      } catch (Exception e) {
+         e.printStackTrace();
+         bodyRequest = "Error al guardar el archivo";
+         response = createHead(500, "text/plain", bodyRequest.length());
+         response += bodyRequest;
+         return response;
+      }
+      
+      return response;
+   }
+   
+   
+   // Las peticiones HTTP DELETE se utilizan para eliminar recursos del servidor. Es una petición idempotente.
+   // Ej: Eliminar un recurso, eliminar un registro de una base de datos, eliminar un archivo, etc.
+   public static String deleteHandler(String resource) {
+      String response = "";
+      String bodyResponse = "";
+      
+      resource = resource.substring(1); // Eliminar la barra inicial
+      System.out.println("Recurso solicitado a eliminar: " + resource);
+      
+      // si en este punto no se entra a ningun if, se verifica si la solicitud implica eliminar un dato de algun archivo txt o json
+      String[] partsOfResource = resource.split("/");
+      int indexResourceFile = 0;
+      String resourceFile = "";
+      String resourceToDelete = "";
+      int index = 1;
+      for (String part : partsOfResource) {
+         resourceFile += part + "/";
+         if (part.contains(".")) {
+            indexResourceFile = index;
+            break;
+         }
+         index++;
+      }
+      resourceFile = resourceFile.substring(0, resourceFile.length() - 1); // Eliminar la última barra
+
+//      System.out.println("partOfResourceFile = " + indexResourceFile);
+//      System.out.println("resourceLength = " + partsOfResource.length);
+      System.out.println("archivo principal = " + resourceFile);
+      
+      // si el archivo principal (con extension) no es el ultimo recurso, debemos eliminar un dato de un archivo txt o json
+      // si el archivo principal (con extension) es el ultimo recurso, se elimina el archivo
+      if (indexResourceFile < partsOfResource.length) {
+         resourceToDelete = partsOfResource[indexResourceFile];
+         System.out.println("archivo a eliminar = " + resourceToDelete);
+         if (resourceFile.endsWith(".txt")) {
+            response = deleteDataFromFile(resourceFile, resourceToDelete);
+            
+         } else if (resourceFile.endsWith(".json")) {
+            if (deleteDataFromJsonFile(new File(resourceFile), resourceToDelete)) {
+               bodyResponse = "Dato eliminado del archivo JSON";
+               response = createHead(200, "text/plain", bodyResponse.length());
+               response += bodyResponse;
+            } else {
+               bodyResponse = "Clave no encontrada en el archivo JSON";
+               response = createHead(404, "text/plain", bodyResponse.length());
+               response += bodyResponse;
+            }
+            
+         } else {
+            bodyResponse = "No se puede eliminar datos de un archivo de este tipo";
+            response = createHead(400, "text/plain", bodyResponse.length());
+            response += bodyResponse;
+         }
+         
+         return response;
+         
+      } else {
+         // Si el recurso solicitado es un directorio, se envía un mensaje de error
+         File file = new File(resource);
+         if (file.isDirectory()) {
+            bodyResponse = "Por medidas de seguridad, no se permite eliminar directorios";
+            response = createHead(400, "text/plain", bodyResponse.length());
+            response += bodyResponse;
+            return response;
+         }
+         
+         // Si el archivo no existe, se envía un mensaje de error
+         if (!file.exists()) {
+            bodyResponse = "Archivo no encontrado";
+            response = createHead(404, "text/plain", bodyResponse.length());
+            response += bodyResponse;
+            return response;
+         }
+         
+         // Si el archivo existe, se elimina
+         if (file.delete()) {
+            bodyResponse = "Archivo eliminado";
+            response = createHead(200, "text/plain", bodyResponse.length());
+            response += bodyResponse;
+            return response;
+         } else {
+            bodyResponse = "Error al eliminar el archivo";
+            response = createHead(500, "text/plain", bodyResponse.length());
+            response += bodyResponse;
+            return response;
+         }
+      }
+   }
+   
+   
+   // Las cabeceras HTTP HEAD son similares a las cabeceras GET, pero no incluyen el cuerpo de la respuesta.
+   // Se utilizan para obtener información sobre un recurso sin tener que recuperar todo el contenido.
+   public static String headHandler(String resource) {
+      String response = "";
+      
+      System.out.println("Petición HEAD con recurso: " + resource);
+      
+      // Si la solicitud es para la raíz o index.html
+      if (resource.equals("/") || resource.equals("/index.html") || resource.equals("/index.htm") || resource == null) {
+         File file = new File("index.html");
+         if (file.exists() && file.isFile()) {
+            response = createHead(200, "text/html", file.length());
+         } else {
+            response = createHead(404, "text/plain", 0);
+         }
+         return response;
+      }
+      
+      // Procesar otros recursos
+      resource = resource.substring(1); // Eliminar la barra inicial
+      System.out.println("Recurso solicitado: " + resource);
+      File file = new File(resource);
+      
+      if (file.isDirectory()) {
+         // Si el recurso es un directorio, devolver un error 403 Forbidden
+         response = createHead(403, "text/plain", 0);
+      } else if (file.exists() && file.isFile()) {
+         // Si el archivo existe, devolver 200 con los detalles
+         String mimeType = MIME_TYPES.get(resource.substring(resource.lastIndexOf(".") + 1));
+         if (mimeType == null) {
+            mimeType = "application/octet-stream"; // Tipo genérico si no está en el mapa
+         }
+         response = createHead(200, mimeType, file.length());
+      } else {
+         // Si el archivo no existe, devolver 404 Not Found
+         response = createHead(404, "text/plain", 0);
+      }
+      
+      return response;
+   }
+   
+   
+   
+   public static String deleteDataFromFile(String fileName, String dataToDelete) {
+      File file = new File(fileName);
+      String response = "";
+      String bodyResponse = "";
+      
+      if (file.exists() && file.isFile()) {
+         // Leer el contenido del archivo
+         String fileContent = "";
+         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+               if (!line.contains(dataToDelete)) { // Si la línea no contiene el dato a eliminar, se agrega al contenido del archivo
+                  fileContent += line + "\n";
+               }
+            }
+         } catch (IOException e) {
+            e.printStackTrace();
+         }
+         
+         // Guardar el contenido actualizado en el archivo
+         try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            writer.write(fileContent);
+         } catch (IOException e) {
+            e.printStackTrace();
+         }
+         
+         bodyResponse = "Datos eliminados del archivo";
+         response = createHead(200, "text/plain", bodyResponse.length());
+         response += bodyResponse;
+      } else {
+         bodyResponse = "Archivo no encontrado";
+         response = createHead(404, "text/plain", bodyResponse.length());
+         response += bodyResponse;
+      }
+      
+      return response;
+   }
+   
+   public static boolean deleteDataFromJsonFile(File file, String keyToDelete) {
+      boolean found = false;
+      
+      try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+         // Leer el contenido del archivo JSON
+         StringBuilder jsonContent = new StringBuilder();
+         String line;
+         
+         while ((line = reader.readLine()) != null) {
+            jsonContent.append(line);
+         }
+         
+         // Parsear el contenido como JSON
+         JsonObject jsonObject = JsonParser.parseString(jsonContent.toString()).getAsJsonObject();
+         
+         // Verificar si la clave existe
+         if (jsonObject.has(keyToDelete)) {
+            jsonObject.remove(keyToDelete); // Eliminar la clave
+            found = true;
+            
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            
+            // Guardar el JSON actualizado en el archivo
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+               writer.write(gson.toJson(jsonObject));
+            }
+         }
+      } catch (IOException e) {
+         e.printStackTrace();
+         return false; // Error durante el proceso
+      }
+      
+      return found; // Devuelve si la clave fue encontrada y eliminada
+   }
+
    
    public static int updateFormSimulation(String form, Map<String, String> parameters) {
       // Abrir el archivo form.txt y gaurdar su contenido en una cadena
